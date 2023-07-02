4a16fcc06029d60f4c4ba939e3263f8bb9f5a189
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-07-02 19:45:09 +0530

    some re-organisation done

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
+use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server =
-        HttpServer::new(|| App::new().service(web::resource("/health_check").to(health_check)))
-            .listen(listener)?
-            .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/health_check").to(health_check))
+            .service(web::resource("/subscriptions").to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
