b662414e3737e52566811d39ab0fe613a579f236
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-07-08 19:32:58 +0530

    chapter 7 done; confirming new subscribers

--- tests/api/subscription.rs
+++ tests/api/subscription.rs
@@ -1,4 +1,3 @@
-use fake::Fake;
 use wiremock::{
     matchers::{method, path},
     Mock, ResponseTemplate,
@@ -21,14 +20,30 @@ async fn subscriber_returns_a_200_for_valid_form_data() {
     let response = app.post_subscription(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+#[actix_web::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
-        .expect("Failed to fetch saved subscription");
+        .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[actix_web::test]
@@ -86,4 +101,24 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
         .await;
 
     app.post_subscription(body.into()).await;
+}
+
+#[actix_web::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    assert_eq!(confirmation_links.plain_text, confirmation_links.html);
 }
